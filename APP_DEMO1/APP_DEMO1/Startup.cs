ca64dbe045b33ea101c91cc78ad655a113a5097a
abdelhadi.erjouany <abdelhadi.erjouany@ABDELHADIERJOU>
2018-04-02 18:17:32 +0100

    commit initial  de peojet             refurturt

---
+++ APP_DEMO1/APP_DEMO1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(APP_DEMO1.Startup))]
+namespace APP_DEMO1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
